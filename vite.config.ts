2e83aa9ae9e626f1fa3beafafadb96ede8dff453
Error430 <itserror430@gmail.com>
2025-01-24 11:53:56 -0500

    Updated things

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,11 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+
+	preview: {
+		host: '0.0.0.0', // Allow external access if needed
+		port: 4173,      // Use the port your server runs on
+		allowedHosts: ['error430.dev'], // Allow specific hostname
+	},
 });
